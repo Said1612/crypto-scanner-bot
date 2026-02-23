b2e0b7f7e3b4ed1126e913dbcc92201766631601
Said1612 <104237115+Said1612@users.noreply.github.com>
2026-02-23 04:16:27 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ CHAT_ID = os.getenv("CHAT_ID")
 BINANCE_URL = "https://data-api.binance.vision/api/v3"
 
 CHECK_INTERVAL = 300
-MIN_VOLUME = 1_000_000
-MIN_CHANGE = 3
+MIN_VOLUME = 500_000
+MIN_CHANGE = 0.5
 
 def send_telegram(message):
     if not TELEGRAM_TOKEN or not CHAT_ID:
